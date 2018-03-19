8de424cb69be632e6eb4a715ed5d10e6ebf90939
Scott Normore <scottnormore@Scotts-MacBook-Pro.local>
2018-03-19 15:10:24 -0230

    game engine can now create objects dynamically. added magic missle, along with some other changes.

--- client/js/entity.js
+++ client/js/entity.js
@@ -7,13 +7,14 @@ function Entity(x,y,id,src,layer,type) {
     this.type=type;
 }
 
+
 //------------------
 
-function playerChar(x,y){
+function playerChar(x,y,layer){
     var id="player";
     var src="images/player/player.png";
-    var layer=2;
-    var type="motion";
+    var layer=3
+    var type="motion"
     Entity.call(this,x,y,id,src,layer,type);
     this.width = 32;
     this.height = 32;
@@ -28,6 +29,7 @@ function playerChar(x,y){
         if(this.cooldown==0){
             if(pressingPower1==true){
                 console.log("Shblam");
+                this.create(new magicMissle(this.x,this.y));
                 this.cooldown=this.cooldown+30
             }
             else if(pressingPower2==true){
@@ -73,11 +75,11 @@ playerChar.prototype.update=function(){
 //
 //  ENVIROMENT TILES
 //
-function grassTile(x,y){
+function grassTile(x,y,layer){
     var id = 'grass';
     var src = 'images/enviroment/tempGrass.png';
-    var layer=1;
-    var type="static";
+    var layer=1
+    var type="static"
     Entity.call(this,x,y,id,src,layer,type);
 }
 grassTile.prototype=Object.create(Entity.prototype);
@@ -86,11 +88,11 @@ grassTile.prototype.update=function(){
     this.remove();
 }
 //-------------
-function dirtTile(x,y){
+function dirtTile(x,y,layer){
     var id = 'dirt';
     var src = 'images/enviroment/tempDirt.png';
-    var layer=1;
-    var type="static";
+    var layer=1
+    var type="static"
     Entity.call(this,x,y,id,src,layer,type);
 }
 dirtTile.prototype=Object.create(Entity.prototype);
@@ -108,10 +110,13 @@ dirtTile.prototype.update=function(){
 block.prototype=Object.create(Entity.prototype);
 block.prototype.constructor = playerChar;
 function block(x,y){
-    Entity.call(this,x,y);
     this.width = 32;
     this.height = 32;
-    this.id="block";
+    id="block";
+    type="static";
+    layer=1
+    src="block url"
+    Entity.call(this,x,y,id,src,layer,type);
     this.update = function(){
     }
     this.draw = function(){
@@ -119,6 +124,27 @@ function block(x,y){
         ctx.fillRect(250+(this.x-player.x),250+(this.y-player.y),32,32);   
     }
     this.collision = function(entityC){
-        this.remove()
+        //this.remove();
+    }
+}
+magicMissle.prototype=Object.create(Entity.prototype);
+magicMissle.prototype.constructor = magicMissle;
+function magicMissle(x,y){
+    this.width = 8;
+    this.height = 8;
+    id="bullet";
+    type="motion";
+    layer=1
+    src="bullet url"
+    Entity.call(this,x,y,id,src,layer,type);
+    this.update = function(){
+        this.x=this.x+3;
+    }
+    this.draw = function(){
+        ctx.fillStyle="#1218E2";
+        ctx.fillRect(250+(this.x-player.x),250+(this.y-player.y),8,8);   
+    }
+    this.collision = function(entityC){
+        this.remove();
     }
 }
--- client/js/gameEngine.js
+++ client/js/gameEngine.js
@@ -37,11 +37,10 @@ document.onkeyup=function(event){
 
 
 //gamestate and or level data
-levelData = function(entityList){
-    this.entityList=entityList;
-    //objects we are always checking
-    //objects that ate static
-    //player inventory?
+levelData = function(motionEntityList,staticEntityList){
+    //this.player=func()
+    this.motionEntityList=motionEntityList;
+    this.staticEntityList=staticEntityList;
 }
 
 //formula for checking if entity one and entity two colides. Returns true if so.
@@ -94,29 +93,49 @@ function gameObject(initialState){
     //this function is the game loop It updates everything in the current game state. 
     this.updateGame=function(){
         ctx.clearRect(0,0,500,500);
-        for(i=0; i < this.currentLevelData.entityList.length ; i++){
-            this.currentLevelData.entityList[i].update();
+        for(i=0; i < this.currentLevelData.motionEntityList.length ; i++){
+            this.currentLevelData.motionEntityList[i].update();
+            //console.log(this.currentLevelData.motionEntityList);
         }
-        for(i=0; i < this.currentLevelData.entityList.length ; i++){
-            for(j=i+1; j < this.currentLevelData.entityList.length ; j++){
-                if(checkCollision(this.currentLevelData.entityList[i],this.currentLevelData.entityList[j])){
+        for(i=0; i < this.currentLevelData.motionEntityList.length ; i++){
+            for(j=i+1; j < this.currentLevelData.motionEntityList.length ; j++){
+                if(checkCollision(this.currentLevelData.motionEntityList[i],this.currentLevelData.motionEntityList[j])){
                     
                     
-                    console.log(this.currentLevelData.entityList[i].id)
                     //entity1Side=checkSide(this.currentLevelData.entityList[i],this.currentLevelData.entityList[j]);
-                    this.currentLevelData.entityList[i].collision(this.currentLevelData.entityList[j]);
+                    this.currentLevelData.motionEntityList[i].collision(this.currentLevelData.motionEntityList[j]);
                     //console.log(entity1Side + "" + this.currentLevelData.entityList[i].id);
                     //entity2Side=checkSide(this.currentLevelData.entityList[j],this.currentLevelData.entityList[i]);
-                    if(this.currentLevelData.entityList[j] == !(null)){
-                        this.currentLevelData.entityList[j].collision(this.currentLevelData.entityList[i]);
+                    if(this.currentLevelData.motionEntityList[j] == !(null)){
+                        this.currentLevelData.motionEntityList[j].collision(this.currentLevelData.motionEntityList[i]);
                         //console.log(entity2Side + "" + this.currentLevelData.entityList[j].id);
                     }
 
                 }
             }
+            for(j=0;j < this.currentLevelData.staticEntityList.length; j++){
+                if(checkCollision(this.currentLevelData.motionEntityList[i],this.currentLevelData.staticEntityList[j])){
+                    //console.log('collision');
+                    
+                    //entity1Side=checkSide(this.currentLevelData.entityList[i],this.currentLevelData.entityList[j]);
+                    this.currentLevelData.motionEntityList[i].collision(this.currentLevelData.staticEntityList[j]);
+                    //console.log(entity1Side + "" + this.currentLevelData.entityList[i].id);
+                    //entity2Side=checkSide(this.currentLevelData.entityList[j],this.currentLevelData.entityList[i]);
+                    if(this.currentLevelData.staticEntityList[j] == !(null)){
+                        //console.log('collision');
+                        this.currentLevelData.staticEntityList[j].collision(this.currentLevelData.motionEntityList[i]);
+                        //console.log(entity2Side + "" + this.currentLevelData.entityList[j].id);
+                    }
+
+                }
+                
+            }
         }
-        for(i=0; i < this.currentLevelData.entityList.length ; i++){
-            this.currentLevelData.entityList[i].draw();
+        for(i=0; i < this.currentLevelData.motionEntityList.length ; i++){
+            this.currentLevelData.motionEntityList[i].draw();
+        }
+        for(i=0; i < this.currentLevelData.staticEntityList.length ; i++){
+            this.currentLevelData.staticEntityList[i].draw();
         }
     }
 
@@ -132,35 +151,56 @@ function gameObject(initialState){
 
 //some code for testing purposes
 
-entityList=[];
+motionEntityList=[];
+staticEntityList=[];
 //entityList.push(new Enemy1(0,0));
 //entityList.push(new Enemy2(200,200));
 player = new playerChar(32,0);
 
-
-entityList.push(new block(0,128));
-entityList.push(new block(32,128));
-entityList.push(new block(64,128));
-entityList.push(new block(96,128));
-entityList.push(new block(128,64));
-entityList.push(new block(128,96));
-entityList.push(new block(128,128));
-entityList.push(new block(160,128));
-entityList.push(new block(192,128));
-entityList.push(new block(224,128));
-entityList.push(new block(224,96));
-entityList.push(new block(256,128));
-entityList.push(new block(288,128));
-entityList.push(new block(320,128));
-entityList.push(player);
+//motionEntityList.push(player);
+staticEntityList.push(new block(0,128));
+staticEntityList.push(new block(32,128));
+staticEntityList.push(new block(64,128));
+staticEntityList.push(new block(96,128));
+staticEntityList.push(new block(128,64));
+staticEntityList.push(new block(128,96));
+staticEntityList.push(new block(128,128));
+staticEntityList.push(new block(160,128));
+staticEntityList.push(new block(192,128));
+staticEntityList.push(new block(224,128));
+staticEntityList.push(new block(224,96));
+staticEntityList.push(new block(256,128));
+staticEntityList.push(new block(288,128));
+staticEntityList.push(new block(320,128));
+motionEntityList.push(player);
 //entityList.push(player);
+//console.log(motionEntityList[0].type);
+//console.log(staticEntityList[0].type);
 
-loadedLevel = new levelData(entityList);
+loadedLevel = new levelData(motionEntityList,staticEntityList);
 game = new gameObject(loadedLevel);
 
 Entity.prototype.remove = function(){
-    var i = game.currentLevelData.entityList.indexOf(this);
-    game.currentLevelData.entityList.splice(i,1);
+    //console.log("collision");
+    if(this.type=="motion"){
+        var i = game.currentLevelData.motionEntityList.indexOf(this);
+        game.currentLevelData.motionEntityList.splice(i,1);
+    }
+    else if (this.type=="static"){
+        var i = game.currentLevelData.staticEntityList.indexOf(this);
+        game.currentLevelData.staticEntityList.splice(i,1);
+    }
+}
+
+//works by pushing the entity in the parameter to the motion/static entity list.
+
+Entity.prototype.create = function(entity){
+    if(entity.type=="motion"){
+        game.currentLevelData.motionEntityList.push(entity);
+    }
+    else if(this.type="static"){
+        game.currentLevelData.staticEntityList.push(entity);
+    }
 }
 
 setInterval(update,1000/60);
